224c620579edc2fe21097e8f26a24dd4a7bcbf35
Mauricio Badilla <mbadilla86@gmail.com>
2021-03-02 10:30:02 -0600

    rails form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+  def form
+    @user = User.new 
+  end
+
+  def create
+    @user = User.new(name:params['name'], email:params['email'])
+    if @user.save
+    redirect_to '/users/index'
+    else
+      render 'form' 
+    end
+  end
+
+  def index
+    @users = User.all
+
+  end
+
+end
