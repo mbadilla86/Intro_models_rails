1ed7b5eb7465e1dbca2afb09be12cb4b7a9afc0a
Mauricio Badilla <mbadilla86@gmail.com>
2021-03-02 12:12:02 -0600

    rails models

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'users/form'
   post 'users/create'
   get 'users/index'
+  get 'users/:id/edit', to: 'users#edit', as: 'users_edit'
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
